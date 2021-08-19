2c82f2fe0074b199a4b4f5260439ce52dfae18d8
Oleksandr Shershak <scherschak@gmail.com>
2021-08-19 09:36:30 +0300

    Refactor Add() method

--- StringCalculator/StringCalc.cs
+++ StringCalculator/StringCalc.cs
@@ -12,14 +12,8 @@ namespace StringCalculator
         {
             if (numbers.Contains('-'))
             {
-                Regex regex = new Regex(@"(-\d*)");
-                string[] negativeNumbers = FindRegex(regex, numbers);
-                string message = "Negatives not allowed: ";
-                foreach (string number in negativeNumbers)
-                {
-                    message += "[" + number + "] ";
-                }
-                throw new Exception(message);
+                string[] negativeNumbers = FindNegativeNumbers(numbers);
+                ShowNegativeNumbers(negativeNumbers);
             }
 
             if (numbers == "")
@@ -30,23 +24,49 @@ namespace StringCalculator
 
             if (numbers.StartsWith('/'))
             {
-                Regex regex = new Regex(@"[[/]*(.*?)[]\n]");
-                string[] delimiters = FindRegex(regex, numbers);
-                string[] numbersArr = numbers.Split("\n", StringSplitOptions.None)[1]
-                                             .Split(delimiters, StringSplitOptions.None);
+                string[] delimiters = FindDelimiters(numbers);
+                string numbersPart = numbers.Split("\n", StringSplitOptions.None)[1];
+                string[] numbersArr = GetNumbers(numbersPart, delimiters);
                 return Sum(numbersArr);
             }
 
             if (numbers.Contains("\n") || numbers.Contains(","))
             {
-                string[] delimeters = new[] { "\n", "," };
-                string[] numbersArr = numbers.Split(delimeters, StringSplitOptions.None);
+                string[] delimiters = new[] { "\n", "," };
+                string[] numbersArr = GetNumbers(numbers, delimiters);
                 return Sum(numbersArr);
             }
             throw new ArgumentException("Incorrect values entered!");
 
         }
-        public string[] FindRegex(Regex regex, string numbers)
+
+        private string[] GetNumbers(string numbers, string[] delimiters)
+        {
+            return numbers.Split(delimiters, StringSplitOptions.None);
+        }
+
+        private string[] FindDelimiters(string numbers)
+        {
+            // Find delimiters between "[" and "]" or between "//" and "\n"
+            Regex regex = new Regex(@"[[/]*(.*?)[]\n]");
+            return FindRegex(regex, numbers);
+        }
+
+        private string[] FindNegativeNumbers(string numbers)
+        {
+            Regex regex = new Regex(@"(-\d*)");
+            return FindRegex(regex, numbers);
+        }
+        private void ShowNegativeNumbers(string[] negativeNumbers)
+        {
+            string message = "Negatives not allowed: ";
+            foreach (string number in negativeNumbers)
+            {
+                message += "[" + number + "] ";
+            }
+            throw new Exception(message);
+        }
+        private string[] FindRegex(Regex regex, string numbers)
         {
             Match match = regex.Match(numbers);
             List<string> matchesList = new List<string>();
@@ -58,7 +78,7 @@ namespace StringCalculator
             string[] matches = matchesList.ToArray();
             return matches;
         }
-        public int Sum(string[] numbersArr)
+        private int Sum(string[] numbersArr)
         {
             foreach (string number in numbersArr)
             {
